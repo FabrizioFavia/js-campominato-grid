761e02504787004af94d87f4266682330f601f1f
FabrizioFavia <personal.favia@gmail.com>
2023-04-26 18:16:17 +0200

    feat(js): created adapting function

--- js/main.js
+++ js/main.js
@@ -1,20 +1,35 @@
 let cellContainer = document.getElementById("grid");
 let startBtn = document.getElementById("start");
+let gridWidth = 49;
 
 
-function difficultySelect(event) {
+function difficultySelect(change) {
 
     let selectElement = document.getElementById("difficultySelect");
-    selectElement = event.target;
+    selectElement = change.target;
     var value = selectElement.value;
-    return value;
+    gridWidth = value;
+    return gridWidth;
 }
 
 startBtn.addEventListener("click", function () {
 
-    for (let i = 1; i <= 64; i++) {
+    for (let i = 1; i <= gridWidth; i++) {
         const cell = document.createElement("div");
-        cell.classList.add("easyCell");
+        cell.classList.add("cell");
+
+        if (gridWidth == 49) {
+            cell.classList.add("easyCell");
+        }
+
+        if (gridWidth == 81) {
+            cell.classList.add("mediumCell");
+        }
+
+        if (gridWidth == 100) {
+            cell.classList.add("hardCell");
+        }
+        
         cell.append(i);
 
         cell.addEventListener("click", function (){
